422a7fcb643aad9b47347981782225523eb9798b
jdganz <jdganzeveld@gmail.com>
2015-10-02 02:01:26 -0700

    first commit

---
+++ src/MergeConflicts.java
@@ -0,0 +1,23 @@
+
+
+// beep boop diddly doop
+public class MergeConflicts {
+	private String name;
+	private boolean bool;
+	
+	public MergeConflicts(){
+		name = "name";
+		bool = true;
+	}
+	
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public boolean getBool(){
+		return this.bool;
+	}
+	
+	
+}
